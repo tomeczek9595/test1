16c006fb36ea5b82408947a26fcdc45d4fc6c1bc
tomeczek9595 <45194246+tomeczek9595@users.noreply.github.com>
2018-11-20 09:00:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<form method="POST" action="typkonta.php">
+<br>
+login:<input type="text" name="login" maxlength="15" size="10"><br>
+haslo:<input type="password" name="haslo" maxlength="15" size="90"><br>
+<input type="submit" value="loguj"/>
+</form>
+<form action="https://serwer1852765.home.pl/Zad 6 wersja 2/usuncookie.php">
+    <input type="submit" value="usun zapisane haslo" />
+</form>
+<form method="POST" action="zarejestruj.php">
+<br>
+login:<input type="text" name="login1" maxlength="15" size="10"><br>
+haslo:<input type="password" name="haslo1" maxlength="15" size="90"><br>
+Powtórzenie hasła:<input type="password" name="haslo2" maxlength="15" size="90"><br>
+<input type="submit" value="rejestruj"/>
+</form>
